a10c862a75a20d5c6763d6ab1a3b2174c6f125ac
nicholasprussen <nicholasprussen@u.boisestate.edu>
2022-01-01 20:56:06 -0800

    Starting new app

---
+++ src/components/App/App.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import Intro from '../Intro/Intro';
+
+const App = (props : any) => {
+    return (
+        <Intro></Intro>
+        // <AboutMe></AboutMe>
+        // <Education></Education>
+        // <WorkExperience></WorkExperience>
+        // <Skills></Skills>
+        // <Portfolio></Portfolio>
+        // <Footer></Footer>
+    );
+}
+
+export default App;
---
+++ src/components/Intro/Intro.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./intro.scss";
+
+const Intro = () => {
+    return (
+        <div className="intro-container">
+            Top
+        </div>
+    );
+}
+
+export default Intro;
